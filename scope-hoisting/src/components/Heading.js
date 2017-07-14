0123d9ac3ec9a53f093d67c12b214227cac599dc
Drew Powers <drew@pow.rs>
2017-07-13 23:49:20 -0400

    Add Scope Hoisting example

---
+++ scope-hoisting/src/components/Heading.js
@@ -0,0 +1,21 @@
+/**
+ * Heading
+ * Big Text
+ */
+
+import styled from 'styled-components';
+
+const Heading = styled.div`
+  background-color: #00ce67;
+  color: white;
+  font-size: 2.4em;
+  font-weight: 700;
+  line-height: 1.2;
+  margin-bottom: 1em;
+  margin-top: 0;
+  padding-bottom: 16px;
+  padding-top: 16px;
+  text-align: center;
+`;
+
+export default Heading;
---
+++ scope-hoisting/src/views/about.js
@@ -0,0 +1,16 @@
+/**
+ * About
+ * The mandatory caboose of every website
+ */
+
+import React from 'react';
+
+import Cell from '../components/Cell';
+
+const About = () => (
+  <Cell>
+    <h2>About</h2>
+  </Cell>
+);
+
+export default About;
