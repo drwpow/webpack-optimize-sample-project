4b055b8e02fa9ed1bdaf409a3204f5e4c8b306d0
Drew Powers <drew@pow.rs>
2017-07-14 01:28:17 -0400

    Extract Nav component

--- 3-dynamic-import/src/index.js
+++ 3-dynamic-import/src/index.js
@@ -14,8 +14,9 @@ import {
 } from 'react-router-dom';
 import styled, { injectGlobal } from 'styled-components';
 
-import Home from './views/home';
 import Countdown from './views/countdown';
+import Home from './views/home';
+import Nav from './components/Nav';
 
 import backgroundImage from './assets/background.svg';
 
@@ -41,40 +42,10 @@ const AppStyles = styled.div`
   font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
 `;
 
-const Nav = styled.nav`
-  background-color: white;
-  box-shadow: 0 2px 4px rgba(0, 8, 16, 0.2);
-  display: flex;
-  margin-bottom: 32px;
-  margin-left: 0;
-  margin-right: 0;
-  padding: 32px;
-`;
-
-const NavLink = styled.div`
-  font-weight: 700;
-  margin-left: 16px;
-  margin-right: 16px;
-
-  & a {
-    color: #00ce67;
-    text-decoration: none;
-    transition: color 200ms;
-
-    &:focus,
-    &:hover {
-      color: #00b85f;
-    }
-  }
-`;
-
 const App = () => (
   <Router>
     <AppStyles>
-      <Nav>
-        <NavLink><Link to="/">Artist Rendering</Link></NavLink>
-        <NavLink><Link to="/countdown">Grand Opening</Link></NavLink>
-      </Nav>
+      <Nav />
 
       <Route exact path="/" component={Home} />
       <Route path="/countdown" component={Countdown} />
